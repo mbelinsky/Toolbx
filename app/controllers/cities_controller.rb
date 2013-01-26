eee70a6a849b3c1062eb458f7000bf60dcf99bce
Paul Straw <paulstraw@paulstraw.com>
2013-01-25 16:31:27 -0800

    City autocomplete cleanup.

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,7 +1,7 @@
 class CitiesController < ApplicationController
   def index
-    unless params[:term] && params[:term].length > 2
-      render text: 'Supply a search term of at least three characters.', status: 400
+    unless params[:term] && params[:term].length > 3
+      render text: 'Supply a search term of at least four characters.', status: 400
       return
     end
 
