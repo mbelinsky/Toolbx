f66369c1cf3a60ca9187c61c888db37b5c24fd06
Paul Straw <paulstraw@paulstraw.com>
2013-02-07 18:19:58 -0800

    Added importer stuff, and fixed the homepage tool order.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     @has_footer = true
     @articles = Article.where(published: true).order('created_at DESC').limit(6)
-    @tools = Tool.order('users_count DESC').order('created_at DESC').limit(6)
+    @tools = Tool.order('created_at DESC').order('users_count DESC').limit(6)
     @categories = Category.all
   end
 
