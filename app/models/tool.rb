b191d02771fc06fe9083ce50057f652e7508404d
Paul Straw <paulstraw@paulstraw.com>
2013-02-17 21:09:00 -0800

    Several tweaks and such to prepare for launch.
    * Updated tool icon sizing logic
    * Added "similar articles" to articles#show
    * Updated /robots.txt to properly block Mr. Roboto from seeing unpublished articles
    * Changed filter text to say "Filter by title…" instead of "Filter by keyword"
    * Added Google+ and LinkedIn links to footer
    * Changed "Closed" license to point to a different Wikipedia article
    * Added licenses to seeds.rb
    * Updated Gemfile to fix a potential New Relic gem issue
    * Tweaked fancybox CSS a bit

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -23,7 +23,7 @@ class Tool < ActiveRecord::Base
   accepts_nested_attributes_for :screens, allow_destroy: true
 
   attr_reader :icon_remote_url
-  has_attached_file :icon, styles: { small: ["14400@", :jpg], thumb: ["3600@", :jpg] }, convert_options: { quality: 85, all: '-background white -mosaic +matte', small: '-extent 120x120', thumb: '-extent 60x60' }
+  has_attached_file :icon, styles: { small: ["120x120", :jpg], thumb: ["60x60", :jpg] }, convert_options: { quality: 85, all: '-background white -mosaic +matte -gravity center', small: '-extent 120x120', thumb: '-extent 60x60' }
   validates_attachment_presence :icon
   validates_attachment_content_type :icon, content_type: ['image/jpg', 'image/jpeg', 'image/pjpeg', 'image/png', 'image/xpng', 'image/gif'], message: 'please upload a jpg, png, or gif file'
   validates_attachment_size :icon, less_than: 1.megabyte
