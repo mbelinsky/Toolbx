3e36f963d0b0cf08ace8c31b6cac2b36e3c97529
Paul Straw <paulstraw@paulstraw.com>
2013-02-17 21:24:55 -0800

    Updated homepage to show featured tools instead of latest.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     @has_footer = true
     @articles = Article.where(published: true).order('created_at DESC').limit(6)
-    @tools = Tool.order('created_at DESC, users_count DESC').limit(6)
+    @tools = Tool.order('featured DESC, users_count DESC, created_at DESC').limit(6)
     @categories = Category.all
   end
 
