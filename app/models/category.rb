d31c1418b92fff0b544d038763a1334bec2995a5
Sean Washington <sean@octopuscreative.com>
2013-11-05 14:24:33 -0800

    Show how many tools are in each category on hover.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,7 @@
 class Category < ActiveRecord::Base
   attr_accessible :title
   validates_presence_of :title
+  has_many :tool_categories, inverse_of: :category, dependent: :destroy
   has_many :tools, through: :tool_categories
   has_many :articles, through: :article_categories
 end
