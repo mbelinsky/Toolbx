a472831ff2e2109e3afafd3c27dafad49ebe4499
Paul Straw <paulstraw@paulstraw.com>
2013-11-15 04:30:00 -0800

    Changed production URLs to point to www.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,8 +48,8 @@ Thetoolbox::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
 
-  config.action_mailer.default_url_options = { host: "thetoolbox.org", protocol: 'https' }
-  config.action_mailer.asset_host = "https://thetoolbox.org"
+  config.action_mailer.default_url_options = { host: "www.thetoolbox.org", protocol: 'https' }
+  config.action_mailer.asset_host = "https://www.thetoolbox.org"
 
   # Enable threaded mode
   # config.threadsafe!
