b64c9455e0e625121702b86a0e27571d2ad576f2
Paul Straw <paulstraw@paulstraw.com>
2013-12-10 11:10:14 -0800

    Rendering custom error pages with correct HTTP status.

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -2,13 +2,16 @@ class ErrorsController < ApplicationController
   def error_404
     @not_found_path = params[:not_found]
     @has_footer = true
+    render 'error_404', status: 404
   end
 
   def error_422
     @has_footer = true
+    render 'error_422', status: 422
   end
 
   def error_500
     @has_footer = true
+    render 'error_500', status: 500
   end
 end
