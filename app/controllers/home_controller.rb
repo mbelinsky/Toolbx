0c5c3e0034716212db5b7bec1b00d55b9ce21b72
Sean Washington <hello@seanwash.com>
2014-03-28 15:03:49 -0700

    Lots of updates to the new homepage.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,12 @@
 class HomeController < ApplicationController
   def index
     @has_footer = true
-    @articles = Article.where(published: true).order('created_at DESC').limit(6)
-    @tools = Tool.order('featured DESC, users_count DESC, created_at DESC').limit(6)
     @categories = Category.all
+
+    tools = Tool.order('featured DESC, users_count DESC, created_at DESC').limit(8).all
+    articles = Article.where(published: true).order('created_at DESC').limit(8).all
+
+    @tools_and_articles = tools.concat(articles).shuffle
   end
 
   def robots
