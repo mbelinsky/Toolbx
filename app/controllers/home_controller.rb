cfa4ecc7511e265b6c63dde60d52dc133e4d5bd5
Paul Straw <paulstraw@paulstraw.com>
2014-04-07 16:11:59 -0700

    Changed home#index tool/article ordering

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,12 +3,16 @@ class HomeController < ApplicationController
     @has_footer = true
     @categories = Category.all
 
-    tools = Tool.order('featured DESC, users_count DESC, created_at DESC').limit(8).all
+    tools = Tool.order('featured DESC, users_count DESC, created_at DESC').limit(8).all.shuffle
     articles = Article.where(published: true).order('created_at DESC').limit(22).all
 
-    #
+    # @tools_and_articles = tools.concat(articles).shuffle
+    @tools_and_articles = []
 
-    @tools_and_articles = tools.concat(articles).shuffle
+    articles.each_with_index do |article, i|
+      @tools_and_articles << article
+      @tools_and_articles << tools[i] if i % 3 == 0
+    end
   end
 
   def robots
