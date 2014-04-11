37280b1d4a31a8140b539db0a57154bf6e3eed77
Paul Straw <paulstraw@paulstraw.com>
2014-04-11 11:09:36 -0700

    Finally fixes the 404 "error"

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,17 +1,21 @@
+# NOTE: Explicit html render to prevent the `ActionView::MissingTemplate`
+# errors when someone requests a non-HTML content type that 404s
+
 class ErrorsController < ApplicationController
   def error_404
     @not_found_path = params[:not_found]
     @has_footer = true
-    render 'error_404', status: 404
+
+    render html: 'error_404', status: 404
   end
 
   def error_422
     @has_footer = true
-    render 'error_422', status: 422
+    render html: 'error_422', status: 422
   end
 
   def error_500
     @has_footer = true
-    render 'error_500', status: 500
+    render html: 'error_500', status: 500
   end
 end
