e91cb65da2b6cd0d19cb63d21c90963c07cb3d28
Paul Straw <paulstraw@paulstraw.com>
2014-04-11 11:18:25 -0700

    More04

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,12 +1,13 @@
-# NOTE: Explicit html render to prevent the `ActionView::MissingTemplate`
-# errors when someone requests a non-HTML content type that 404s
-
 class ErrorsController < ApplicationController
   def error_404
     @not_found_path = params[:not_found]
     @has_footer = true
 
-    render html: 'error_404', status: 404
+    # NOTE: Explicit `respond_to` to prevent the `ActionView::MissingTemplate`
+    # errors when someone requests a non-HTML content type that 404s
+    respond_to do |format|
+      format.html { render 'error_404', status: 404 }
+    end
   end
 
   def error_422
